112058d9e68d0f86ca1d0df5cc9ec8c0eb783351
yu <1426202+yswallow@users.noreply.github.com>
2020-09-19 19:49:18 +0900

    improbed error logging

--- code.py
+++ code.py
@@ -1,23 +1,34 @@
 import sys
+import board
+import supervisor
+import os
 
 try:
+    supervisor.set_rgb_status_brightness(8)
     from cpkf.cpkf import CPKFKeyboard
     import keyboard
-    import keymap
-
-    kbd = CPKFKeyboard(keymap=keyboard.layouts(keymap.keymap), scan_method=keyboard.Scan.scan)
+    if "keymap.py" in os.listdir():
+        import keymap
+        kbd = CPKFKeyboard(keymap=keyboard.layouts(keymap.keymap), scan_method=keyboard.Scan.scan)
+    else:
+        kbd = CPKFKeyboard(keymap=keyboard.layouts(keyboard.default_keymap.keymap), scan_method=keyboard.Scan.scan)
 
     kbd.start()
 
 except Exception as err:
-    sys.print_exception(err)
-    
     try:
         kbd.release_all();
+    except NameError as err2:
+        print("Keyboard not Running.")
+    else:
+        pass
+    
+    try:
         with open("/log/error.txt", "w") as fp:
             sys.print_exception(err, fp)
     except OSError as err2:
         print("Can't write file.")
-        sys.print_exception(err2)
     else:
         raise
+
+    raise(err)
