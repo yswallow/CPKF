8ab8c1985bb19a370c6906c077a7f721bd0e4608
yu <1426202+yswallow@users.noreply.github.com>
2020-09-19 19:48:41 +0900

    add keycodes for japanese

---
+++ lib/CPKF/keycode_jp.py
@@ -0,0 +1,41 @@
+from cpkf.key_object import S
+
+class KeycodeJP:
+    
+    MINUS = 0x2D
+    EQUALS = S(0x2D)
+    
+    CIRC = 0x2E
+    CIRCUMFLEX = 0x2E
+    TILDE = S(0x2E)
+    
+    JPYEN = 0x89
+    PIPE = S(0x89)
+
+    ATMARK = 0x2F
+    GRAVE_ACCENT =  S(0x2F)
+    
+    LEFT_BRACKET = 0x30
+    LEFT_CURLY_BRACKET = S(0x30)
+    
+    SEMICOLON = 0x33
+    PLUS = S(0x33)
+    
+    COLON = 0x34
+    ASTERISK = S(0x34)
+    
+    RIGHT_BRACKET = 0x35
+    RIGHT_CURLY_BRACKET = S(0x35)
+    
+    COMMA = 0x36
+    LEFT_ANGLE_BRACKET = S(0x36)
+
+    PERIOD = 0x37
+    RIGHT_ANGLE_BRACKET = S(0x37)
+
+    FORWARD_SLASH = 0x38
+    QUESTION = S(0x38)
+
+    BACK_SLASH = 0x87
+    UNDERSCORE = S(0x87)
+    UNDERBAR = S(0x87)
