99d6e3f765d98235d6deb7128ca00dfabd6da8fb
yu <1426202+yswallow@users.noreply.github.com>
2020-12-11 04:33:13 +0900

    refactored

--- lib/CPKF/mousekey.py
+++ lib/CPKF/mousekey.py
@@ -1,105 +1,61 @@
 from cpkf.key_object import KeyObject
 from config import MOUSEKEY_CONFIG
 
-class MOUSEU(KeyObject):
-    def __init__(self):
-        pass
-    
+class MOUSEKey(KeyObject):
     def press(self, kbd, time):
+        if kbd.mouse is None:
+            kbd.updateHIDdevice()
         self.t = time
-    
-    def release(self, kbd, time):
-        pass
-    
+
+class MOUSEU(MOUSEKey):
     def tick(self, kbd, time):
         if(time-self.t > MOUSEKEY_CONFIG["INTERVAL"]):
             kbd.mouse.move(x=0, y=-MOUSEKEY_CONFIG["MAX_SPEED"])
             self.t = time
 
-class MOUSED(KeyObject):
-    def __init__(self):
-        pass
-    
-    def press(self, kbd, time):
-        self.t = time
-    
-    def release(self, kbd, time):
-        pass
-    
+class MOUSED(MOUSEKey):
     def tick(self, kbd, time):
         if(time-self.t > MOUSEKEY_CONFIG["INTERVAL"]):
             kbd.mouse.move(x=0, y=MOUSEKEY_CONFIG["MAX_SPEED"])
             self.t = time
 
-class MOUSEL(KeyObject):
-    def __init__(self):
-        pass
-    
-    def press(self, kbd, time):
-        self.t = time
-    
-    def release(self, kbd, time):
-        pass
-    
+class MOUSEL(MOUSEKey):
     def tick(self, kbd, time):
         if(time-self.t > MOUSEKEY_CONFIG["INTERVAL"]):
             kbd.mouse.move(x=-MOUSEKEY_CONFIG["MAX_SPEED"], y=0)
             self.t = time
 
-class MOUSER(KeyObject):
-    def __init__(self):
-        pass
-    
-    def press(self, kbd, time):
-        self.t = time
-    
-    def release(self, kbd, time):
-        pass
-    
+class MOUSER(MOUSEKey):
     def tick(self, kbd, time):
         if(time-self.t > MOUSEKEY_CONFIG["INTERVAL"]):
             kbd.mouse.move(x=MOUSEKEY_CONFIG["MAX_SPEED"], y=0)
             self.t = time
 
-class WHEELU(KeyObject):
-    def __init__(self):
-        pass
-    
-    def press(self, kbd, time):
-        self.t = time
-    
-    def release(self, kbd, time):
-        pass
-    
+class WHEELU(MOUSEKey):
     def tick(self, kbd, time):
         if(time-self.t > MOUSEKEY_CONFIG["INTERVAL"]):
             kbd.mouse.move(wheel=MOUSEKEY_CONFIG["WHEEL_MAX_SPEED"])
             self.t = time
 
-class WHEELD(KeyObject):
-    def __init__(self):
-        pass
-    
-    def press(self, kbd, time):
-        self.t = time
-    
-    def release(self, kbd, time):
-        pass
-    
+class WHEELD(MOUSEKey):
     def tick(self, kbd, time):
         if(time-self.t > MOUSEKEY_CONFIG["INTERVAL"]):
             kbd.mouse.move(wheel=-MOUSEKEY_CONFIG["WHEEL_MAX_SPEED"])
             self.t = time
 
-class MOUSELB(KeyObject):
-    def __init__(self):
-        pass
-
+class MOUSELB(MOUSEKey):
     def press(self, kbd, time):
+        super().press(kbd, time)
         kbd.mouse.press(kbd.mouse.LEFT_BUTTON)
     
     def release(self, kbd, time):
         kbd.mouse.release(kbd.mouse.LEFT_BUTTON)
+
+
+class MOUSERB(MOUSEKey):
+    def press(self, kbd, time):
+        super().press(kbd, time)
+        kbd.mouse.press(kbd.mouse.RIGHT_BUTTON)
     
-    def tick(self, kbd, time):
-        pass
+    def release(self, kbd, time):
+        kbd.mouse.release(kbd.mouse.RIGHT_BUTTON)
