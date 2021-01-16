1621b0748cd1d038b01f63fb33e6c58f7997c165
yu <1426202+yswallow@users.noreply.github.com>
2021-01-17 04:49:05 +0900

    Impl: add accel feature to mousekey

--- lib/CPKF/mousekey.py
+++ lib/CPKF/mousekey.py
@@ -5,42 +5,58 @@ class MOUSEKey(KeyObject):
     def press(self, kbd, time):
         if kbd.mouse is None:
             kbd.updateHIDdevice()
+        self.count = 0
         self.t = time
+    
+    def getSpeed(self):
+        if(self.count < MOUSEKEY_CONFIG["TIME_TO_MAX"]):
+            self.count += 1
+            return int(self.count*MOUSEKEY_CONFIG["MAX_SPEED"]/MOUSEKEY_CONFIG["TIME_TO_MAX"])
+        else:
+            return MOUSEKEY_CONFIG["MAX_SPEED"]
 
+class WHEELKey(MOUSEKey):
+    def getSpeed(self):
+        if(self.count < MOUSEKEY_CONFIG["WHEEL_TIME_TO_MAX"]):
+            self.count += 1
+            return int(self.count*MOUSEKEY_CONFIG["WHEEL_MAX_SPEED"]/MOUSEKEY_CONFIG["WHEEL_TIME_TO_MAX"])
+        else:
+            return MOUSEKEY_CONFIG["WHEEL_MAX_SPEED"]
+            
 class MOUSEU(MOUSEKey):
     def tick(self, kbd, time):
         if(time-self.t > MOUSEKEY_CONFIG["INTERVAL"]):
-            kbd.mouse.move(x=0, y=-MOUSEKEY_CONFIG["MAX_SPEED"])
+            kbd.mouse.move(x=0, y=-self.getSpeed())
             self.t = time
 
 class MOUSED(MOUSEKey):
     def tick(self, kbd, time):
         if(time-self.t > MOUSEKEY_CONFIG["INTERVAL"]):
-            kbd.mouse.move(x=0, y=MOUSEKEY_CONFIG["MAX_SPEED"])
+            kbd.mouse.move(x=0, y=self.getSpeed())
             self.t = time
 
 class MOUSEL(MOUSEKey):
     def tick(self, kbd, time):
         if(time-self.t > MOUSEKEY_CONFIG["INTERVAL"]):
-            kbd.mouse.move(x=-MOUSEKEY_CONFIG["MAX_SPEED"], y=0)
+            kbd.mouse.move(x=-self.getSpeed(), y=0)
             self.t = time
 
 class MOUSER(MOUSEKey):
     def tick(self, kbd, time):
         if(time-self.t > MOUSEKEY_CONFIG["INTERVAL"]):
-            kbd.mouse.move(x=MOUSEKEY_CONFIG["MAX_SPEED"], y=0)
+            kbd.mouse.move(x=self.getSpeed(), y=0)
             self.t = time
 
 class WHEELU(MOUSEKey):
     def tick(self, kbd, time):
         if(time-self.t > MOUSEKEY_CONFIG["INTERVAL"]):
-            kbd.mouse.move(wheel=MOUSEKEY_CONFIG["WHEEL_MAX_SPEED"])
+            kbd.mouse.move(wheel=self.getSpeed())
             self.t = time
 
-class WHEELD(MOUSEKey):
+class WHEELD(WHEELKey):
     def tick(self, kbd, time):
         if(time-self.t > MOUSEKEY_CONFIG["INTERVAL"]):
-            kbd.mouse.move(wheel=-MOUSEKEY_CONFIG["WHEEL_MAX_SPEED"])
+            kbd.mouse.move(wheel=-self.getSpeed())
             self.t = time
 
 class MOUSELB(MOUSEKey):
