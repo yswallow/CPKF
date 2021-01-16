a24425695d8e11752d17cf65e5eb962db6d11734
yu <1426202+yswallow@users.noreply.github.com>
2021-01-17 05:40:48 +0900

    modified config.py value

--- config.py
+++ config.py
@@ -4,9 +4,9 @@ TAPPING_TERM_FORCE_HOLD = True
 MOUSEKEY_CONFIG = {
     "DELAY": 0.05,
     "INTERVAL": 0.02,
-    "MAX_SPEED": 10,
+    "MAX_SPEED": 40,
     "TIME_TO_MAX": 10,
-    "WHEEL_MAX_SPEED": 1,
-    "WHEEL_TIME_TO_MAX": 30,
+    "WHEEL_MAX_SPEED": 3,
+    "WHEEL_TIME_TO_MAX": 10,
 }
 
